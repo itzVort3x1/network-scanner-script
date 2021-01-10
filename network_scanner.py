7ffb409e3c03a72a703bfb266eac6be7282ad48b
kaustubh <saikaustubh10@gmail.com>
2021-01-10 09:15:22 +0530

    added scanner

--- network_scanner.py
+++ network_scanner.py
@@ -5,6 +5,12 @@ import scapy.all as scapy
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
-    print(arp_request.summary())
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    # scapy.ls(scapy.Ether) ; used to list all the arguments of a function
+    # using forward slash("/") we can combine both packets into one packet
+    arp_request_bradcast = broadcast/arp_request
+    answered, unanswered = scapy.srp(arp_request_bradcast, timeout=1)
+    print(answered.summary())
+
 
 scan("10.0.2.5")
